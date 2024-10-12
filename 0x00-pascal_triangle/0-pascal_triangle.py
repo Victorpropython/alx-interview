4e854a2f334fcd1f127a610effc037ef6008aa9e
victorpropython <victorchibuzor3109@gmail.com>
2024-10-12 15:54:42 +0100

    for pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -16,5 +16,6 @@ def pascal_triangle(n):
         for j in range(1, i):  # Fill in the middle elements
             row[j] = triangle[i-1][j-1] + triangle[i-1][j]
         triangle.append(row)
- return triangle
+    
+    return triangle
 
