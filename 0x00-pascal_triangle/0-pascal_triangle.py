29b838c0281bb6eb24af4a61877dfd0f122fa300
victorpropython <victorchibuzor3109@gmail.com>
2024-10-12 15:50:47 +0100

    for pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,14 +5,16 @@
 """
 
 def pascal_triangle(n):
+
     """
     Function for processing the data
     """
+
     triangle = []
     for i in range(n):# Loop to generate each row
         row = [1] * (i + 1)  # Start with all 1s
         for j in range(1, i):  # Fill in the middle elements
             row[j] = triangle[i-1][j-1] + triangle[i-1][j]
         triangle.append(row)
-    return triangle
+        return triangle
 
