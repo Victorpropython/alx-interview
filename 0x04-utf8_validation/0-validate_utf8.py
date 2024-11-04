2f3eadf2c8216c5599fa53e1d8bc1c4dc5296c6b
victorpropython <victorchibuzor3109@gmail.com>
2024-11-04 05:02:30 +0100

    for task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,48 +1,40 @@
 #!/usr/bin/python3
-""" Code to validate UTF-8 Encoding
-"""
+""" UTF-8 Validation """
 
 
 def validUTF8(data):
-    """FUnction to validate The list of bytes
-
-    Args:
-        data: Used for tracking the number of arguments in
-        a list
-    Returns:
-        To return a  valid UTF-8 code
     """
-    num_in_bytes = 0
+    Method that determines if a given data set represents a valid
+    UTF-8 encoding.
+    """
+    number_bytes = 0
 
-    num1 = 1 << 7  # This is for 128 bit tracking 10000000
-    num2 = 1 << 6  # This is for 64 bit tracking  01000000
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-    for nums in data:
-        byte = nums & 0xff  # This is to get the least Significant bit
+    for i in data:
 
-        if num_in_bytes == 0:
-            if (byte & num1) == 0:
-                # This is for one byte character
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
                 continue
 
-            elif (byte & num1) and (byte & num2):
-                num = num1
-                print("before while", num)
-                while (byte & num):
-                    num_in_bytes += 1
-                    num >>= 1
-
-                # Making sure UTF-8 is not more than 4 bytes
-                if num_in_bytes == 1 or num_in_bytes > 4:
-                    print("before false", num_in_bytes)
-                    return False
-            else:
+            if number_bytes == 1 or number_bytes > 4:
                 return False
+
         else:
-            if not (byte & num1 and not (byte & num2)):
-                return False
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
 
-        num_in_bytes -= 1
-        print(num_in_bytes)
+        number_bytes -= 1
 
-    return num_in_bytes == 0  # To return True if all bytes matched pattern
+    if number_bytes == 0:
+        return True
+
+    return False
---
+++ 0x04-utf8_validation/vicc.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Code to validate UTF-8 Encoding
+"""
+
+
+def validUTF8(data):
+    """FUnction to validate The list of bytes
+
+    Args:
+        data: Used for tracking the number of arguments in
+        a list
+    Returns:
+        To return a  valid UTF-8 code
+    """
+    num_in_bytes = 0
+
+    num1 = 1 << 7  # This is for 128 bit tracking 10000000
+    num2 = 1 << 6  # This is for 64 bit tracking  01000000
+
+    for nums in data:
+        byte = nums: & 0xff  # This is to get the least Significant bit
+
+        if num_in_bytes == 0:
+            if (byte & num1) == 0:
+                # This is for one byte character
+                continue
+
+            elif (byte & num1) and (byte & num2):
+                num = num1
+                print("before while", num)
+                while (byte & num):
+                    num_in_bytes += 1
+                    num >>= 1
+
+                # Making sure UTF-8 is not more than 4 bytes
+                if num_in_bytes == 1 or num_in_bytes > 4:
+                    print("before false", num_in_bytes)
+                    return False
+            else:
+                return False
+        else:
+            if not (byte & num1 and not (byte & num2)):
+                return False
+
+        num_in_bytes -= 1
+        print(num_in_bytes)
+
+    return num_in_bytes == 0  # To return True if all bytes matched pattern
