b11b3b277f715fc13195f6d91408ac3844a97559
victorpropython <victorchibuzor3109@gmail.com>
2024-11-04 04:40:46 +0100

    for the first task

---
+++ 0x04-utf8_validation/vic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+mask1 = 1<<7
+mask2 = 1<<6
+mask3 = 1<<5
+mask4 = 1<<4
+mask5 = 1<<3
+mask6 = 1<<9
+
+print((mask1 & 0xff) ,mask2, mask3,mask4, mask5, (mask6 & 0xff), end=" ")
